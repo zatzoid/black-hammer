391c7b06d0e138cc33ed5e960f47e78b74794144
zatzoid <zatzoid@gmail.com>
2024-09-08 19:37:17 +0300

    slider

--- js/blogSlider.js
+++ js/blogSlider.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", function () {
-
-    const container = document.querySelector('.blog__items')
-    const items = container.querySelectorAll('.blog__item')
+    const container = document.querySelector('.blog__wrp')
+    const itemlist = container.querySelector('.blog__items')
+    const items = itemlist.querySelectorAll('.blog__item')
 
     let touchStart = 0
     let currentSlide = 0
@@ -9,7 +9,7 @@ window.addEventListener("DOMContentLoaded", function () {
     container.addEventListener('touchstart', (ev) => { touchStart = ev.touches[0].clientX })
     container.addEventListener('touchend', (ev) => {
 
-        const maxSlide = window.innerWidth > 1340 ? null : window.innerWidth > 1000 ? 1 : 2
+        const maxSlide = window.innerWidth > 1340 ? 1 : window.innerWidth > 1000 ? 2 : 3
         const endX = ev.changedTouches[0].clientX
 
         if (touchStart + 50 > endX && currentSlide < maxSlide) {
@@ -21,16 +21,35 @@ window.addEventListener("DOMContentLoaded", function () {
             currentSlide--
 
         } else {
-
-
             return
         }
 
         touchStart = 0
-        container.style.transform = `translateX(calc(-${items[0].getBoundingClientRect().width * currentSlide}px - ${30 * currentSlide}px))`
+        itemlist.style.transform = `translateX(calc(-${items[0].getBoundingClientRect().width * currentSlide}px - ${30 * currentSlide}px))`
+    })
+
+
+    container.addEventListener('wheel', (ev) => {
+        const maxSlide = window.innerWidth > 1340 ? 1 : window.innerWidth > 1000 ? 1 : 2
+        const endX = ev.deltaY
+
+        if (1 > endX && currentSlide < maxSlide) {
+            currentSlide++
+
+
+
+        } else if (1 < endX && currentSlide > 0) {
+            currentSlide--
+
+        } else {
+            return
+        }
+
+      
+        itemlist.style.transform = `translateX(calc(-${items[0].getBoundingClientRect().width * currentSlide}px - ${30 * currentSlide}px))`
     })
     window.addEventListener('resize', () => {
-        container.style.transform = `translateX(0)`
+        itemlist.style.transform = `translateX(0)`
         currentSlide = 0
     })
 
